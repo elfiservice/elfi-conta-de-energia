fd5545c5ee406f9cd18f0ac589df9dc11a604b7c
Armando Jr <elfiservice@hotmail.com>
2019-01-09 11:03:52 -0300

    feat: show slides on screen and some styles

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
 
 class Slides extends Component {
+    renderSlides = () => {
+        console.log(this.props.data);
+        
+        return this.props.data.map( (slide, index) => (
+            <View key={index} style={styles.slide} >
+                <Text style={styles.slideText}>{slide.text}</Text>
+            </View>
+        ))
+    }
     render() {
         return (
-           <View>
-               <Text>  </Text>
-           </View>
+            <ScrollView
+                horizontal
+                style={{ flex: 1 }}
+            >
+                {this.renderSlides()}
+            </ScrollView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    slide: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    slideText: {
+        fontSize: 20
+    }
+});
 
 export default Slides
