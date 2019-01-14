9937e7f601e237f5532619e9407b39bc363c89cf
Armando Jr <elfiservice@hotmail.com>
2019-01-14 16:11:30 -0300

    styles: removed some comments

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -3,20 +3,14 @@ import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native'
 import { blue, white, red } from '../utils/colors'
 import { Button } from 'react-native-elements'
 
-// import DiaFechamentoFatura from './DiaFechamentoFatura'
-// import LeituraAnterior from './LeituraAnterior'
-// import TarifaAtual from './TarifaAtual'
 import Slide from './Slide'
 
 const SCREEN_WIDTH_DEVICE = Dimensions.get('window').width;
 
 class Slides extends Component {
     _renderSlides = () => {
-        //console.log(this.props.data);
-        
         return this.props.data.map( (slide, index) => (
             <View key={index} style={[styles.slide, { backgroundColor: slide.color }]} >
-                {/* {this._selectSlide(slide)} */}
                 <Slide slide={slide} />
                 {this._renderLastSlide(index)}
             </View>
@@ -32,23 +26,6 @@ class Slides extends Component {
                     onPress={this.props.onComplete}
                 />
             )
-        }
-    }
-    _selectSlide(slide) {
-        switch (slide.id) {
-            case 1: 
-                return <DiaFechamentoFatura slide={slide} />
-                break;
-            case 2: 
-                return <LeituraAnterior slide={slide} />
-                break;
-            case 3: 
-                return <TarifaAtual slide={slide} />
-                break;
-        
-            default:
-                return <DiaFechamentoFatura slide={slide} />
-                break;
         }
     }
     render() {
